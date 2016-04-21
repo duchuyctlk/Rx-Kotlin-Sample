cccdb77daf89054ff03e869ff341d282b83b033f
HuyDuc <huy.duc@eastagile.com>
2016-04-21 15:22:00 +0700

    [c] Config CircleCI

--- app/src/main/java/ea/huyduc/rxkotlinsample/MainActivity.kt
+++ app/src/main/java/ea/huyduc/rxkotlinsample/MainActivity.kt
@@ -1,7 +1,10 @@
 package ea.huyduc.rxkotlinsample
 
+import android.app.KeyguardManager
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 /**
  * Created by huyduc on 4/20/16.
@@ -9,6 +12,14 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (BuildConfig.DEBUG) {
+            val km : KeyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+            val keyguardLock : KeyguardManager.KeyguardLock = km.newKeyguardLock(MainActivity::class.simpleName)
+            keyguardLock.disableKeyguard()
+            window.addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON)
+        }
+
         setContentView(R.layout.activity_main)
     }
 }
